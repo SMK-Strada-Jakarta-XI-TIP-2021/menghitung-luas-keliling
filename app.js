4e326f9266c1ce21c13b5e18f9a0b9094052e65c
limmi26 <kisamakiazuel@gmail.com>
2021-08-04 11:43:14 +0700

    perubahan 1

---
+++ app.js
@@ -0,0 +1,67 @@
+// fungsi luas lingkaran
+function luasLingkaran(jari) {
+    return 3.14 * jari * jari;
+}
+
+// fungsi keliling lingkaran
+function kelilingLingkaran(jari) {
+    return 2 * 3.14 * jari;
+}
+
+// jari jari 10
+console.log("jari-jari = 10");
+
+// luas lingkaran
+var r = 10;
+var luas = luasLingkaran(10);
+console.log('luas = ' + luas);
+
+// keliling lingkaran
+var r = 10;
+var keliling = Math.round(kelilingLingkaran(10));
+console.log('keliling = ' + keliling);
+
+console.log(); // pembatas
+
+// jari jari 8
+console.log("jari-jari = 8");
+
+// luas lingkaran
+var r = 8;
+var luas = luasLingkaran(8);
+console.log('luas = ' + luas); // hasil luas
+
+// keliling lingkaran
+var r = 8;
+var keliling = Math.round(kelilingLingkaran(8));
+console.log('keliling = ' + keliling); // hasil keliling
+
+console.log(); // pembatas
+
+// jari jari 7
+console.log("jari-jari = 7");
+
+// luas lingkaran
+var r = 7;
+var luas = luasLingkaran(7);
+console.log('luas= ' + luas); // hasil luas
+
+//keliling lingkaran
+var r = 7;
+var keliling = Math.round(kelilingLingkaran(7));
+console.log('keliling = ' + keliling); // hasil keliling
+
+console.log(); // pembatas
+
+// jari jari 5
+console.log("jari-jari = 5");
+
+// luas lingkaran
+var r = 5;
+var luas = luasLingkaran(5);
+console.log('luas = ' + luas); // hasil luas
+
+// keliling lingkaran
+var r = 5;
+var keliling = Math.round(kelilingLingkaran(5));
+console.log('keliling = ' + keliling); // hasil keliling
