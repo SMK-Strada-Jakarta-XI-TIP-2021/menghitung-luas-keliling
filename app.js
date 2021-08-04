68c447a38889dede1eb5559147e7996f068d8760
novi <novia@gmail.com>
2021-08-04 10:52:02 +0700

    tugas novi

---
+++ app.js
@@ -0,0 +1,55 @@
+// kerjakan soal disini
+// fungsi untuk menghitung luas lingkaran
+function luasLingkaran(jari2) {
+    return 3.14 * jari2 * jari2;
+}
+
+// fungsi untuk menghitung keliling lingkaran
+function kelilingLingkaran(jari2) {
+    return 2 * 3.14 * jari2;
+}
+
+// jari-jari 10
+console.log("             ");
+console.log("jari-jari= 10");
+var r = 10
+var luas = luasLingkaran(r);
+console.log('luas= ' + luas);
+
+var r = 10
+var keliling = Math.round(kelilingLingkaran(r));
+console.log('keliling= ' + keliling);
+
+
+// jari-jari 8
+console.log("             ");
+console.log("jari-jari= 8");
+var r = 8
+var luas = luasLingkaran(r);
+console.log('luas= ' + luas);
+
+var r = 8
+var keliling = Math.round(kelilingLingkaran(r));
+console.log('keliling= ' + keliling);
+
+// jari-jari 7
+console.log("             ");
+console.log("jari-jari= 8");
+var r = 7
+var luas = luasLingkaran(r);
+console.log('luas= ' + luas);
+
+var r = 7
+var keliling = Math.round(kelilingLingkaran(r));
+console.log('keliling= ' + keliling);
+
+// jari-jari 5
+console.log("             ");
+console.log("jari-jari= 5");
+var r = 5
+var luas = luasLingkaran(r);
+console.log('luas= ' + luas);
+
+var r = 5
+var keliling = Math.round(kelilingLingkaran(r));
+console.log('keliling= ' + keliling);
