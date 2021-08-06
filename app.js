778b6ade784741ea6447bf96355a3f8b9e24d739
adrigunawan <adrigunawan32@gmail.com>
2021-08-06 09:54:46 +0700

    Perubahan kedua

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-function kelilingLingkaran (a, b) {
-    var pi;
-    var d;
-    var r;
-    var k;
-
-    pi = a;
-    r = b;
-    k = 2 * a * b;
-    d = b / 2;
+function luasLingkaran (jari) {
+    if (jari % 7 === 0) {
+        luaslingkaranPertama = Math.round (jari * jari * 3.14);
+        return luaslingkaranPertama;
+    } else {
+        luaslingkaranKedua = jari * jari * 3.14;
+        return luaslingkaranKedua
+    }
 }
 
-console.log('jari-jari 1' + ' = ' + kelilingLingkaran(10, 3.14))
+function kelilingLingkaran (jari) {
+    if (jari % 7 === 0) {
+        kelilinglingkaranPertama = Math.round (2 * jari * 3.14);
+        return kelilinglingkaranPertama;
+    
+    } else {
+        kelilinglingkaranKedua = 2 * jari * 3.14;
+        return kelilinglingkaranKedua;
+    }
+}
+
+console.log ('1 .' + 'Luas = ' + luasLingkaran(10))
+console.log ('keliling = ' + kelilingLingkaran(10))
+console.log ('2 .' + 'Luas = ' + luasLingkaran(8))
+console.log ('keliling = ' + kelilingLingkaran(8))
+console.log ('3 .' + 'Luas = ' + luasLingkaran(7))
+console.log ('keliling = ' + kelilingLingkaran(7))
+console.log ('4 .' + 'Luas = ' + luasLingkaran(5))
+console.log ('keliling = ' + kelilingLingkaran(5))
