96e2d0264bf3e7a1124d1e370b18b40dc29d1bab
jlerapatty <lerapattyjoaquin@gmail.com>
2021-08-08 20:39:00 +0700

    Joaquin

---
+++ app.js
@@ -0,0 +1,69 @@
+// Area Of Circle
+function circleArea(radius) {
+    return Math.PI * radius * radius;
+}
+
+// Circumference
+function circumference(radius) {
+    return 2 * Math.PI * radius; 
+}
+
+// RADIUS 10
+console.log('')
+console.log("============")
+console.log("Jari-jari 10")
+
+// Area 
+var r = 10
+var area = circleArea(r);
+console.log('area = ' + area) 
+
+// Circum
+var r = 10
+var circum = Math.round(circumference(r));
+console.log('circum = ' + circum ) 
+
+// RADIUS 8
+console.log('')
+console.log("============")
+console.log("Jari-jari 8")
+
+// Area 
+var r = 8
+var area = circleArea(r);
+console.log('area = ' + area) 
+
+// Circum
+var r = 8
+var circum = Math.round(circumference(r));
+console.log('circum = ' + circum ) 
+
+// RADIUS 7
+console.log('')
+console.log("============")
+console.log("Jari-jari 7")
+
+// Area 
+var r = 7
+var area = circleArea(r);
+console.log('area = ' + area) 
+
+// Circum
+var r = 7
+var circum = Math.round(circumference(r));
+console.log('circum = ' + circum ) 
+
+// RADIUS 5
+console.log('')
+console.log("============")
+console.log("Jari-jari 5")
+
+// Area 
+var r = 5
+var area = circleArea(r);
+console.log('area = ' + area) 
+
+// Circum
+var r = 5
+var circum = Math.round(circumference(r));
+console.log('circum = ' + circum ) 
